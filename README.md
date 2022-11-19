009ffdbd62d8c73fa704d5694920cb08eb2db079
Luis Mamani Lingan <72869012@certus.edu.pe>
2022-11-18 22:55:16 -0500

    finish report

--- README.md
+++ README.md
@@ -384,7 +384,7 @@ define(["N/search", "N/format", "N/log"], function (search, format, log) {
 });
 ```
 
-## REPORTES (6/7)
+## REPORTES (7/7)
 
 ### 1) Cuales son los Récords que usa todo Report Generator de cualquier reporte de cualquier país; y cuál es el uso de cada uno de ellos?
 
@@ -414,7 +414,205 @@ Para considerar el proceso de **'Special Period'** dentro de un reporte se deben
 
 ### 5) Implementar una búsqueda por script del Catálogo de Cuenta de cualquier país. Columnas a considerar : Nro (Correlativo), Número de Cuenta, Nombre
 
----
+```javascript
+/* 
+Implementar una búsqueda por script del Catálogo de Cuenta de cualquier país. 
+Columnas a considerar : Nro (Correlativo), Número de Cuenta, Nombre
+*/
+
+define(["N/search", "N/log"], function (search, log) {
+  var LMRY_SCRIPT_NAME = "LMRY - Exercise 4";
+
+  /**
+   * @type {Array<string>}
+   */
+  var subsidiariesGroup = [];
+
+  /**
+   * @type {Array<{correlative:string,accountId:string,accountNumber:string|'- None -',accountName:string}>}
+   */
+  var accountsData = [];
+
+  /** - Obtiene todas las subsidiarias de un país
+   * @description Obtiene todas las subsidiarias de un país
+   * @param {string} countryCode - Código del país
+   * @returns {void} - No retorna nada pero guarda los datos en la variable subsidiariesGroup
+   */
+  function _getSubsidiariesByCountry(countryCode) {
+    try {
+      var DbolStop = false;
+      var minInterval = 0;
+      var maxInterval = 1000;
+
+      var SC_Filters = [
+        search.createFilter({
+          name: "country",
+          operator: search.Operator.ANYOF,
+          values: [countryCode],
+        }),
+      ];
+
+      var SC_Columns = [
+        search.createColumn({
+          name: "internalid",
+          summary: search.Summary.GROUP,
+          label: "0. LatamReady - Internal ID",
+        }),
+      ];
+
+      var SC_Search = search.create({
+        type: search.Type.SUBSIDIARY,
+        filters: SC_Filters,
+        columns: SC_Columns,
+      });
+
+      var searchResult = SC_Search.run();
+
+      var auxResult;
+      while (!DbolStop) {
+        auxResult = searchResult.getRange(minInterval, maxInterval);
+        if (auxResult != null) {
+          if (auxResult.length != 1000) DbolStop = true;
+          for (var i = 0; i < auxResult.length; i++) {
+            var columns = auxResult[i].columns;
+
+            var subsidiaryId = auxResult[i].getValue(columns[0]);
+
+            subsidiariesGroup.push(subsidiaryId);
+          }
+          minInterval = maxInterval;
+          maxInterval = maxInterval + 1000;
+        } else {
+          DbolStop = true;
+        }
+      }
+    } catch (error) {
+      log.error("Error in _getSubsidiariesByCountry", {
+        title: "[ MPRD - _getSubsidiariesByCountry ]",
+        message: error,
+        relatedScript: LMRY_SCRIPT_NAME,
+      });
+    }
+  }
+
+  /** - Obtiene todas las cuentas de subisidiarias de un determinado pais
+   * @description Obtiene todas las cuentas de subisidiarias de un determinado pais
+   * @param {string} countryCode - Código del país
+   * @returns {void} - No retorna nada pero guarda datos en la variable accountsData
+   */
+  function _getAccountsByCountry(countryCode) {
+    try {
+      var DbolStop = false;
+      var minInterval = 0;
+      var maxInterval = 1000;
+
+      _getSubsidiariesByCountry(countryCode);
+      if (subsidiariesGroup.length == 0) accountsData = [];
+
+      var AC_Filters = [
+        search.createFilter({
+          name: "subsidiary",
+          operator: search.Operator.ANYOF,
+          values: subsidiariesGroup,
+        }),
+      ];
+
+      var AC_Columns = [
+        search.createColumn({
+          name: "internalid",
+          summary: search.Summary.GROUP,
+          label: "0. LatamReady - Internal ID",
+        }),
+        search.createColumn({
+          name: "number",
+          summary: search.Summary.GROUP,
+          label: "1. LatamReady - Account Number",
+        }),
+        search.createColumn({
+          name: "displayname",
+          summary: search.Summary.GROUP,
+          label: "2. LatamReady - Account Name",
+        }),
+      ];
+
+      var IT_Search = search.create({
+        type: search.Type.ACCOUNT,
+        filters: AC_Filters,
+        columns: AC_Columns,
+      });
+
+      var searchResult = IT_Search.run();
+
+      var auxResult;
+      var countCorrelative = 1;
+      while (!DbolStop) {
+        auxResult = searchResult.getRange(minInterval, maxInterval);
+        if (auxResult != null) {
+          if (auxResult.length != 1000) DbolStop = true;
+          for (var i = 0; i < auxResult.length; i++) {
+            var columns = auxResult[i].columns;
+
+            var accountId = auxResult[i].getValue(columns[0]);
+            var accountNumber = auxResult[i].getValue(columns[1]);
+            var accountName = auxResult[i].getValue(columns[2]);
+
+            accountsData.push({
+              correlative: _completeCorrelative(6, countCorrelative),
+              accountId: accountId,
+              accountNumber: accountNumber,
+              accountName: accountName,
+            });
+
+            countCorrelative++;
+          }
+          minInterval = maxInterval;
+          maxInterval = maxInterval + 1000;
+        } else {
+          DbolStop = true;
+        }
+      }
+    } catch (error) {
+      log.error("Error in _getAccountsByCountry", {
+        title: "[ MPRD - _getAccountsByCountry ]",
+        message: error,
+        relatedScript: LMRY_SCRIPT_NAME,
+      });
+    }
+  }
+
+  /** - Agrega ceros a la izquierda según el tamaño del string y el tamaño deseado
+   * @description - Agrega ceros a la izquierda según el tamaño del string y el tamaño deseado
+   * @param {number} size - Tamaño del string
+   * @param {string} value - String a formatear
+   * @returns {string} - String formateado con ceros a la izquierda
+   */
+  function _completeCorrelative(size, value) {
+    try {
+      var valueLength = ("" + value).length;
+      if (("" + value).length <= size) {
+        if (size != ("" + value).length) {
+          for (var i = ("" + value).length; i < size; i++) {
+            value = "0" + value;
+          }
+        } else {
+          return value;
+        }
+        return value;
+      } else {
+        value = value.substring(valueLength - size, valueLength);
+        return value;
+      }
+      return value;
+    } catch (error) {
+      log.error("Error in _completeCorrelative", {
+        title: "[ SCHDL - _completeCorrelative ]",
+        message: error,
+        relatedScript: LMRY_SCRIPT_NAME,
+      });
+    }
+  }
+});
+```
 
 ### 6) Una búsqueda de transacción (Invoice) puede traer hasta 1.000.000 de registros por mes, al momento de intentar realizar la búsqueda para recuperar la información, esta se cae por 'tiempo de ejecución excedido'. Cual seria su solución para corregir este problema (Implementar código)
 
--- src/exercises/exercise-4.js
+++ src/exercises/exercise-4.js
@@ -1,62 +1,55 @@
 /* 
-Una búsqueda de transacción (Invoice) puede traer hasta 1.000.000 de registros por mes, 
-al momento de intentar realizar la búsqueda para recuperar la información, 
-esta se cae por 'tiempo de ejecución excedido'. 
-Cual seria su solución para corregir este problema (Implementar código).
-*/
-
-/* La solución esta en hacer un bucle que vaya de 1000 en 1000, y 
-que vaya guardando los resultados en un array, esto para evitar
-la iteración de 1.000.000 de lineas que provoca que caiga el search, y luego retornar ese array,
-en casos donde se usen SCHDL o MPRC se pueden hacer rellamados al script
+Implementar una búsqueda por script del Catálogo de Cuenta de cualquier país. 
+Columnas a considerar : Nro (Correlativo), Número de Cuenta, Nombre
 */
 
 define(["N/search", "N/log"], function (search, log) {
-  var LMRY_SCRIPT_NAME = "LMRY - Exercise 3";
+  var LMRY_SCRIPT_NAME = "LMRY - Exercise 4";
 
   /**
-   * @type {Array<{tranId:string,tranDate:string}>}
+   * @type {Array<string>}
    */
-  var rta = [];
+  var subsidiariesGroup = [];
 
-  /** - Obtiene todos los registros de Invoice
-   * @description Obtiene todos los registros de Invoice
-   * @returns {void} - No retorna nada pero guarda datos en la variable rta
+  /**
+   * @type {Array<{correlative:string,accountId:string,accountNumber:string|'- None -',accountName:string}>}
    */
-  function _getInvoices() {
+  var accountsData = [];
+
+  /** - Obtiene todas las subsidiarias de un país
+   * @description Obtiene todas las subsidiarias de un país
+   * @param {string} countryCode - Código del país
+   * @returns {void} - No retorna nada pero guarda los datos en la variable subsidiariesGroup
+   */
+  function _getSubsidiariesByCountry(countryCode) {
     try {
       var DbolStop = false;
       var minInterval = 0;
       var maxInterval = 1000;
 
-      var IT_Filters = [
+      var SC_Filters = [
         search.createFilter({
-          name: "type",
+          name: "country",
           operator: search.Operator.ANYOF,
-          values: ["CustInvc"],
+          values: [countryCode],
         }),
       ];
 
-      var IT_Columns = [
+      var SC_Columns = [
         search.createColumn({
           name: "internalid",
           summary: search.Summary.GROUP,
           label: "0. LatamReady - Internal ID",
         }),
-        search.createColumn({
-          name: "trandate",
-          summary: search.Summary.GROUP,
-          label: "1. LatamReady - Transaction Date",
-        }),
       ];
 
-      var IT_Search = search.create({
-        type: search.Type.TRANSACTION,
-        filters: IT_Filters,
-        columns: IT_Columns,
+      var SC_Search = search.create({
+        type: search.Type.SUBSIDIARY,
+        filters: SC_Filters,
+        columns: SC_Columns,
       });
 
-      var searchResult = IT_Search.run();
+      var searchResult = SC_Search.run();
 
       var auxResult;
       while (!DbolStop) {
@@ -66,13 +59,9 @@ define(["N/search", "N/log"], function (search, log) {
           for (var i = 0; i < auxResult.length; i++) {
             var columns = auxResult[i].columns;
 
-            var tranId = auxResult[i].getValue(columns[0]);
-            var tranDate = auxResult[i].getValue(columns[1]);
+            var subsidiaryId = auxResult[i].getValue(columns[0]);
 
-            rta.push({
-              tranId: tranId,
-              tranDate: tranDate,
-            });
+            subsidiariesGroup.push(subsidiaryId);
           }
           minInterval = maxInterval;
           maxInterval = maxInterval + 1000;
@@ -81,8 +70,125 @@ define(["N/search", "N/log"], function (search, log) {
         }
       }
     } catch (error) {
-      log.error("Error in _getInvoices", {
-        title: "[ MPRD - _getInvoices ]",
+      log.error("Error in _getSubsidiariesByCountry", {
+        title: "[ MPRD - _getSubsidiariesByCountry ]",
+        message: error,
+        relatedScript: LMRY_SCRIPT_NAME,
+      });
+    }
+  }
+
+  /** - Obtiene todas las cuentas de subisidiarias de un determinado pais
+   * @description Obtiene todas las cuentas de subisidiarias de un determinado pais
+   * @param {string} countryCode - Código del país
+   * @returns {void} - No retorna nada pero guarda datos en la variable accountsData
+   */
+  function _getAccountsByCountry(countryCode) {
+    try {
+      var DbolStop = false;
+      var minInterval = 0;
+      var maxInterval = 1000;
+
+      _getSubsidiariesByCountry(countryCode);
+      if (subsidiariesGroup.length == 0) accountsData = [];
+
+      var AC_Filters = [
+        search.createFilter({
+          name: "subsidiary",
+          operator: search.Operator.ANYOF,
+          values: subsidiariesGroup,
+        }),
+      ];
+
+      var AC_Columns = [
+        search.createColumn({
+          name: "internalid",
+          summary: search.Summary.GROUP,
+          label: "0. LatamReady - Internal ID",
+        }),
+        search.createColumn({
+          name: "number",
+          summary: search.Summary.GROUP,
+          label: "1. LatamReady - Account Number",
+        }),
+        search.createColumn({
+          name: "displayname",
+          summary: search.Summary.GROUP,
+          label: "2. LatamReady - Account Name",
+        }),
+      ];
+
+      var IT_Search = search.create({
+        type: search.Type.ACCOUNT,
+        filters: AC_Filters,
+        columns: AC_Columns,
+      });
+
+      var searchResult = IT_Search.run();
+
+      var auxResult;
+      var countCorrelative = 1;
+      while (!DbolStop) {
+        auxResult = searchResult.getRange(minInterval, maxInterval);
+        if (auxResult != null) {
+          if (auxResult.length != 1000) DbolStop = true;
+          for (var i = 0; i < auxResult.length; i++) {
+            var columns = auxResult[i].columns;
+
+            var accountId = auxResult[i].getValue(columns[0]);
+            var accountNumber = auxResult[i].getValue(columns[1]);
+            var accountName = auxResult[i].getValue(columns[2]);
+
+            accountsData.push({
+              correlative: _completeCorrelative(6, countCorrelative),
+              accountId: accountId,
+              accountNumber: accountNumber,
+              accountName: accountName,
+            });
+
+            countCorrelative++;
+          }
+          minInterval = maxInterval;
+          maxInterval = maxInterval + 1000;
+        } else {
+          DbolStop = true;
+        }
+      }
+    } catch (error) {
+      log.error("Error in _getAccountsByCountry", {
+        title: "[ MPRD - _getAccountsByCountry ]",
+        message: error,
+        relatedScript: LMRY_SCRIPT_NAME,
+      });
+    }
+  }
+
+  /** - Agrega ceros a la izquierda según el tamaño del string y el tamaño deseado
+   * @description - Agrega ceros a la izquierda según el tamaño del string y el tamaño deseado
+   * @param {number} size - Tamaño del string
+   * @param {string} value - String a formatear
+   * @returns {string} - String formateado con ceros a la izquierda
+   */
+  function _completeCorrelative(size, value) {
+    try {
+      var valueLength = ("" + value).length;
+      if (("" + value).length <= size) {
+        if (size != ("" + value).length) {
+          for (var i = ("" + value).length; i < size; i++) {
+            value = "0" + value;
+          }
+        } else {
+          return value;
+        }
+        return value;
+      } else {
+        value = value.substring(valueLength - size, valueLength);
+        return value;
+      }
+      return value;
+    } catch (error) {
+      log.error("Error in _completeCorrelative", {
+        title: "[ SCHDL - _completeCorrelative ]",
         message: error,
         relatedScript: LMRY_SCRIPT_NAME,
       });
